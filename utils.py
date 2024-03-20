f31182ecfb56e357f496bf75a22d17e9253499a0
Hassan-Tamer <hassantamerha@gmail.com>
2024-03-20 16:03:37 +0200

    utils class

---
+++ utils.py
@@ -0,0 +1,66 @@
+import os 
+from pydub import AudioSegment
+from pytube import Playlist
+from pytube import YouTube
+
+class utils:
+
+    def get_total_duration(self, path ,verbose=False):
+        """
+        Returns the total length of all the wav files in the directory
+        """
+        list_dir = os.listdir(path)
+        length = 0
+        for url in list_dir:
+            if url.endswith('.wav'):
+                audio = AudioSegment.from_file(url, format="wav")
+                length += len(audio)
+        if verbose:
+            print(f"Total length of all wav files in the directory: {length/1000/60} minutes")
+        return length/1000/60
+    
+    def download_video(self,url,trim=3):
+        """
+        Downloads the video from the given URL and trims the audio by {trim} minutes from the start and end
+        trim: int, default=3
+        """
+        try:
+            video = YouTube(url)
+            stream = video.streams.filter(only_audio=True).first()
+            file_path = stream.download(filename=f"{video.title}.mp4")
+            print(f"{video.title} is downloaded in WAV format")
+            try:
+                audio = AudioSegment.from_file(file_path, format="mp4")
+            except FileNotFoundError:
+                print("Error: File not found")
+                return
+            if len(audio) < trim * 2  * 60 * 1000:
+                print(f"The audio is less than {trim * 2} minutes long.")
+                os.remove(file_path)
+                return
+            
+            start_time = trim * 60 * 1000  # Convert trim minutes to milliseconds
+            end_time = len(audio) - (trim * 60 * 1000)
+            trimmed_audio = audio[start_time:end_time]
+            trimmed_file_path = f"{video.title}.wav"
+            trimmed_audio.export(trimmed_file_path, format="wav")
+            print(f"Trimmed audio saved as {trimmed_file_path}")
+        except KeyError:
+            print("Unable to fetch video information. Please check the video URL or your network connection.")
+    
+    def download_playlist(self,URL_PLAYLIST,trim=3):
+        """
+        Downloads all the videos in the playlist and trims the audio by {trim} minutes from the start and end
+        trim: int, default=3
+        """
+        playlist = Playlist(URL_PLAYLIST)
+        print(f'Number Of Videos In playlist:{len(playlist.video_urls)}')
+        urls = []
+        for url in playlist:
+            urls.append(url)
+            
+        for i, url in enumerate(urls):
+            print(f"Downloading video {i+1} of {len(urls)}")
+            print(url)
+            self.download_video(url,trim)
+            print("\n\n")
